1ba251d7f1a3939588f89f29605acbd5f81e7f2f
CabbageNom <calumcalder@hotmail.co.uk>
2014-06-03 21:50:23 +0100

    Add UID generator

--- src/game/Entities.java
+++ src/game/Entities.java
@@ -5,4 +5,10 @@ import java.util.ArrayList;
 public class Entities {
 	public static ArrayList<Ship> ships = new ArrayList<Ship>();
 	public static ArrayList<Missile> missiles = new ArrayList<Missile>();
+	
+	private static int uID = 0;
+	public int getUID() {
+		uID++;
+		return uID;
+	}
 }
